54064871a749e92042f6fab597ba712da645adb6
Sheriev Akhmed <sherievakhmed@live.ru>
2018-10-05 03:00:37 +0300

    initialize solution analyzer

--- SolutionAnalyzer/Program.cs
+++ SolutionAnalyzer/Program.cs
@@ -1,15 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace SolutionAnalyzer
 {
+    public class ProjectNode
+    {
+        public string RootProject { get; set; }
+        public List<ProjectNode> ChildProjects { get; set; } = new List<ProjectNode>();
+    }
+
     class Program
     {
+        static Dictionary<string, ProjectNode> hash = new Dictionary<string, ProjectNode>();
+
+
         static void Main(string[] args)
         {
+            var fileName = @"c:\test\DocumentServices.sln";
+            var projectLinks = SolutionParser.ParseFile(fileName);
+
+            var di = new FileInfo(fileName);
+
+            foreach (var item in projectLinks)
+            {
+                var projectFullPath = Path.Combine(di.DirectoryName, item);
+                Console.WriteLine(projectFullPath);
+
+                AddSelectedNode("solution", projectFullPath);
+
+                var projectFile = new FileInfo(projectFullPath);
+
+                var subProjectLinks = ProjectParser.ParseFile(projectFullPath);
+                foreach (var subProjectLink in subProjectLinks)
+                {
+                    var subprojectFullPath = Path.GetFullPath(Path.Combine(projectFile.DirectoryName, subProjectLink));
+                    AddSelectedNode(projectFullPath, subprojectFullPath);
+                    Console.WriteLine($"   {subprojectFullPath}");
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void AddSelectedNode(string projectFullPath, string childFile)
+        {
+            var rootNode = GetdNode(projectFullPath);
+
+            var childNode = GetdNode(childFile);
+
+            rootNode.ChildProjects.Add(childNode);
+        }
+
+        private static ProjectNode GetdNode(string file)
+        {
+            if (!hash.ContainsKey(file))
+            {
+                hash[file] = new ProjectNode()
+                {
+                    RootProject = file,
+                    ChildProjects = new List<ProjectNode>()
+                };
+            }
+
+            return hash[file];
+        }
+    }
+
+    public class ProjectParser
+    {
+        public static IEnumerable<string> ParseFile(string projectFullPath)
+        {
+            var xDocument = XDocument.Load(projectFullPath);
+            var subProjectReferences = xDocument.Descendants().Where(i => i.Name.LocalName == "ProjectReference");
+            var files = subProjectReferences.Select(n =>
+                  n.Attributes().Where(i => i.Name.LocalName == "Include").Select(i => i.Value).First());
+            return files;
         }
     }
 }
---
+++ SolutionAnalyzer/SolutionParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SolutionAnalyzer
+{
+    internal class SolutionParser
+    {
+        internal static IEnumerable<string> ParseFile(string fileName)
+        {
+            var slnFileLines = File.ReadAllLines(fileName);
+            var projectLines = slnFileLines.Where(i => i.StartsWith("Project") && i.Contains(".csproj"));
+            var projectItems = projectLines.Select(i => i.Split('=')[1].Replace("\"","").Split(','));
+
+            return projectItems.Select(i => i[1].Trim());
+            //return projectItems.Select(i => new ProjectItem()
+            //{
+            //    ProjectName=i[0],
+            //    FileName = i[1]
+            //});
+
+
+        }
+    }
+
+    public class ProjectItem
+    {
+        public string FileName { get; set; }
+        public string ProjectName { get; set; }
+    }
+}
